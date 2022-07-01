5f2459a5fe6588062ea4b55d3866ca18d36c7ad3
SSergeyA <93119897+SSergeyA@users.noreply.github.com>
2022-07-01 15:29:37 +0300

    Update s3.tf

--- virt/14/s3.tf
+++ virt/14/s3.tf
@@ -4,8 +4,8 @@ terraform {
      bucket = "netology-bucket-test"
      region = "ru-central1-a"
      key = "terraform.tfstate"
-     access_key = "YCAJEYylkEeiCNPPOGVds6jg7"
-     secret_key = "YCPztfMcUf_YhzCgQ7Tm9JQ____qSd58FFo_3wmj"
+     access_key = "YCA6jg7"
+     secret_key = "YCPwmj"
   
      skip_region_validation = true
      skip_credentials_validation = true
